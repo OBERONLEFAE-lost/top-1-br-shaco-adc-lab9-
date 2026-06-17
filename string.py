80f852963018d9c1fc78cfdf855ababa2f7ae8d4
OBERON <samueljogar.sr@gmail.com>
2026-06-17 17:16:16 -0300

    as parada ai

---
+++ string.py
@@ -0,0 +1,10 @@
+n = input("qual nome?")
+s = input("qual sobrenome?")
+print(n +" "+ s)
+
+frase = input("digite uma frase:")
+print(frase.count(" "))
+
+print("seu nome é", n)
+for i in range(n):
+    print(n[i])
