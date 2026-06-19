bf3a8163416fd56deaeeb34df2ba0b1a49b15a76
OBERON <samueljogar.sr@gmail.com>
2026-06-19 11:25:57 -0300

    ate a 5
    o rato roeu a roupa do rei de noxus

--- string.py
+++ string.py
@@ -2,9 +2,34 @@ n = input("qual nome?")
 s = input("qual sobrenome?")
 print(n +" "+ s)
 
+
 frase = input("digite uma frase:")
 print(frase.count(" "))
 
+
 print("seu nome é", n)
-for i in range(n):
-    print(n[i])
+resto = ""
+for letra in n:
+    resto += letra
+    print(resto)
+
+
+numero = input("digite um numero:")
+if len(numero) == 8:
+    numero = "9" + numero
+    print(numero[0:5] + "-" + numero[5:9])
+elif len(numero) == 9:
+    if numero[0] == "9":
+        print(numero[0:5] + "-" + numero[5:9])
+    else:
+        print("numero invalido")
+
+
+frase = input("digite uma frase:")
+vogais = frase.count("a") + frase.count("e") + frase.count("i") + frase.count("o") + frase.count("u")
+print("a frase tem", vogais, "vogais")
+indiceVOGAIS = []
+for i in range(len(frase)):
+    if frase[i] in "aeiou":
+        indiceVOGAIS.append(i)
+print("os indices das vogais são:", indiceVOGAIS)
